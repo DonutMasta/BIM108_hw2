0666474684de351e3dbef8b260c6b79d4fc9e23b
DonutMasta <94189068+DonutMasta@users.noreply.github.com>
2023-05-09 11:46:25 +0300

    first commit

---
+++ Department.java
@@ -0,0 +1,33 @@
+
+public class Department {
+
+	protected String name;
+	protected int numberOfWorker;
+	public static int number;
+
+	// Do not change anything in this method
+	public String getName() {
+		return name;
+	}
+
+	// Fill in the blanks. 
+	public Department(String name, int numberOfEmployee) {
+		//Write your codes here
+	}
+
+	// Erase the content. Fill in the blanks. If name and numberOfWorker are equal, it returns true
+	@Override
+	public boolean equals(Object obj) {
+		//Write your codes here
+		return false; // don't forget to delete this line
+	}
+
+	// Erase the content. Fill in the blanks. Format: [name]/[numberOfWorker]
+	// For example: Android/number of worker : 30
+	@Override
+	public String toString() {
+		//Write your codes here
+		return ""; // don't forget to delete this line
+	}
+
+}
